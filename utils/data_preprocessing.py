8a071b29b9a56e6661f5f6b1ae96b33898fa1e9d
Arek Kesiz Abnousi <99053976+Arek-KesizAbnousi@users.noreply.github.com>
2024-12-03 17:48:25 -0500

    Update data_preprocessing.py

--- utils/data_preprocessing.py
+++ utils/data_preprocessing.py
@@ -3,10 +3,13 @@
 import cudf
 
 def preprocess_data():
+    # Read ingested data
     gdf = cudf.read_csv('data/processed/mnist_ingested.csv')
+
     # Normalize pixel values
     pixel_columns = [str(i) for i in range(28*28)]
     gdf[pixel_columns] = gdf[pixel_columns] / 255.0
+
     # Save preprocessed data
     gdf.to_csv('data/processed/mnist_preprocessed.csv', index=False)
 
